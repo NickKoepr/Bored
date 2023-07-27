9d9235fefdca35a766538d42174475d8309f5abf
Nick <nickkuiper44@gmail.com>
2023-07-27 12:24:13 +0200

    Changed the default values to null for arguments (needed for the filters later on)

--- app/src/main/java/nl/nickkoepr/bored/model/Arguments.kt
+++ app/src/main/java/nl/nickkoepr/bored/model/Arguments.kt
@@ -1,10 +1,10 @@
 package nl.nickkoepr.bored.model
 
 data class Arguments(
-    val minPrice: Double = 0.0,
-    val maxPrice: Double = 1.0,
-    val minAccessibility: Double = 0.0,
-    val maxAccessibility: Double = 1.0,
+    val minPrice: Double? = null,
+    val maxPrice: Double? = null,
+    val minAccessibility: Double? = null,
+    val maxAccessibility: Double? = null,
     val participants: Int? = null,
-    val type: Type = Type.NONE
+    val type: Type? = null
 )
