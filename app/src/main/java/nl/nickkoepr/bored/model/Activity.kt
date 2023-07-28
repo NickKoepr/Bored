37ca68fbc8e68fee577c64c88657ff5694bb9c03
Nick <65495045+NickKoepr@users.noreply.github.com>
2023-07-28 15:55:28 +0200

    Changed link to be an non nullable value (because the api always returns a link value, empty when not available)

--- app/src/main/java/nl/nickkoepr/bored/model/Activity.kt
+++ app/src/main/java/nl/nickkoepr/bored/model/Activity.kt
@@ -22,14 +22,14 @@ data class Activity(
     val type: Type,
     val participants: Int,
     @FloatRange(0.0, 1.0) val price: Double,
-    val link: String?
+    val link: String
 )
 
 object DummyActivities {
     val activities = listOf(
-        Activity("0", "Volunteer and help out at a senior center", 1.0, Type.BUSYWORK, 1, 1.0, null),
-        Activity("1", "Learn how to make an Alexa skill", 1.0, Type.BUSYWORK, 1, 1.0, null),
-        Activity("2", "Make a bucket list", 1.0, Type.BUSYWORK, 1, 1.0, null),
-        Activity("3", "test", 1.0, Type.BUSYWORK, 1, 1.0, null)
+        Activity("0", "Volunteer and help out at a senior center", 1.0, Type.BUSYWORK, 1, 1.0, ""),
+        Activity("1", "Learn how to make an Alexa skill", 1.0, Type.BUSYWORK, 1, 1.0, ""),
+        Activity("2", "Make a bucket list", 1.0, Type.BUSYWORK, 1, 1.0, ""),
+        Activity("3", "test", 1.0, Type.BUSYWORK, 1, 1.0, "")
     )
 }
