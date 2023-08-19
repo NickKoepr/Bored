82c535b34d48510955ff386ebfa8f8bd90f9f5e7
Nick <65495045+NickKoepr@users.noreply.github.com>
2023-08-19 20:07:11 +0200

    Changed some small things inside the favorite card and added comments to the composable functions.

--- app/src/main/java/nl/nickkoepr/bored/ui/screens/favorites/FavoritesScreen.kt
+++ app/src/main/java/nl/nickkoepr/bored/ui/screens/favorites/FavoritesScreen.kt
@@ -5,13 +5,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -39,14 +40,24 @@ fun FavoritesScreen(
 ) {
     LazyColumn(modifier = modifier) {
         items(DummyActivities.activities) { activity ->
-            FavoriteCard(activity = activity, modifier = Modifier.padding(bottom = 15.dp))
+            FavoriteCard(
+                activity = activity,
+                modifier = Modifier.padding(bottom = 15.dp, start = 12.dp, end = 12.dp)
+            )
         }
     }
 }
 
+/**
+ * Card that displays the given activity title with the activity stats (only the icon and the stat).
+ * @param activity activity that has to be displayed on the card.
+ */
 @Composable
 fun FavoriteCard(activity: Activity, modifier: Modifier = Modifier) {
-    Card(modifier = modifier) {
+    Card(
+        modifier = modifier,
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primaryContainer)
+    ) {
         Column(
             modifier = Modifier.padding(
                 start = 15.dp,
@@ -59,12 +70,19 @@ fun FavoriteCard(activity: Activity, modifier: Modifier = Modifier) {
                 activity = activity,
                 textStyle = MaterialTheme.typography.displayMedium
             )
-            Spacer(modifier = Modifier.height(20.dp))
+            Divider(
+                color = MaterialTheme.colorScheme.onTertiaryContainer,
+                modifier = Modifier.padding(top = 14.dp, bottom = 10.dp)
+            )
             ActivityStatsSmallOverview(activity = activity)
         }
     }
 }
 
+/**
+ * Row containing all the different activity stats with an icon and the given stat.
+ * @param activity activity whose status should be shown.
+ */
 @Composable
 fun ActivityStatsSmallOverview(activity: Activity, modifier: Modifier = Modifier) {
     Row(modifier = modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
@@ -87,6 +105,11 @@ fun ActivityStatsSmallOverview(activity: Activity, modifier: Modifier = Modifier
     }
 }
 
+/**
+ * Stat item that displays the given selectedFilter icon and the given value.
+ * @param selectedFilter the type of filter that will be shown on the stat item.
+ * @param value value of the selectedFilter type.
+ */
 @Composable
 fun ActivityStatsSmallItem(
     selectedFilter: SelectedFilter,
@@ -102,12 +125,13 @@ fun ActivityStatsSmallItem(
             tint = MaterialTheme.colorScheme.primary,
             modifier = Modifier.size(30.dp)
         )
-        Spacer(modifier = Modifier.width(5.dp))
+        Spacer(modifier = Modifier.width(2.dp))
         Text(
             text = value,
             style = MaterialTheme.typography.labelSmall,
             fontSize = 16.sp,
-            fontWeight = FontWeight.Bold
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colorScheme.onSurfaceVariant
         )
     }
 }
