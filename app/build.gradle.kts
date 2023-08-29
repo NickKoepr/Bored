7f9a28120c268f95e004984bbbc3b38e1f601d3c
Nick <65495045+NickKoepr@users.noreply.github.com>
2023-08-29 21:25:23 +0200

    Replaced the accompanist systemuicontroller with enableEdgeToEdge.
    the systemuitcontroller api is deprecated in upcoming versions.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.activity:activity-compose:1.8.0-alpha07")
 
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
@@ -66,7 +66,6 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
     implementation("androidx.navigation:navigation-compose:2.7.1")
     implementation("androidx.compose.material3:material3-window-size-class:1.1.1")
-    implementation("com.google.accompanist:accompanist-systemuicontroller:0.33.1-alpha")
     implementation("com.google.android.gms:play-services-oss-licenses:17.0.1")
 
     // Retrofit
