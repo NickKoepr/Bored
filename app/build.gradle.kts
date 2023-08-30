cde2e8e7dbf0187004166364e40d45e01506ab67
Nick <65495045+NickKoepr@users.noreply.github.com>
2023-08-30 15:56:18 +0200

    Changed version name to 1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 29
         targetSdk = 34
         versionCode = 1
-        versionName = "0.1"
+        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
